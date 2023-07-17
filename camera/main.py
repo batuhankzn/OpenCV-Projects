3333b36347afc5ea0d83d7d53536e67402d08eba
Batuhan Kazan <batuhankzn61@gmail.com>
2023-07-17 19:12:17 +0300

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -2,6 +2,14 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+##dizinden video ekleyip gösterebiliriz
+#cap = cv2.VideoCapture("video.mp4")
+
+##video bittikten sonra hata vermeden ekranı kapatır
+# if ret == 0:
+#    break
+
+
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
